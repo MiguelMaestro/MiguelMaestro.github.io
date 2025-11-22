db0dc9266fbc7c57ae195ae2e56c9ad797f79e0c
Miguel Maestro <miguel.angel.maestro.martinez1#kyndryl.com>
2025-11-22 16:58:47 +0100

    Feat: Configure GitHub Pages deployment with Static Export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/new_portfolio",
+  assetPrefix: "/new_portfolio",
 };
 
 export default nextConfig;
